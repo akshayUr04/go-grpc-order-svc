1e164fdd2d48ea1b77bc6fbe0030c822233c70ef
akshayUr04 <akshayur0404@gmail.com>
2023-05-12 09:01:46 +0530

    pb

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,25 @@
 package configs
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Port          string `mapstruct:"PORT"`
+	DBUrl         string `mpastruct:"DB_URL"`
+	ProductSvcUrl string `mapstruct:"PRODUCT_SVC_URL"`
+}
+
+func LoadConfig() (config Config, err error) {
+	viper.AddConfigPath("./pkg/config/envs")
+	viper.SetConfigName("dev")
+	viper.SetConfigType("env")
+
+	viper.ReadInConfig()
+
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+
+	return
+}
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,1 +1,24 @@
 package db
+
+import (
+	"log"
+
+	"github.com/akshayUr04/go-grpc-order-svc/pkg/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	DB *gorm.DB
+}
+
+func Init(url string) Handler {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&models.Order{})
+	return Handler{}
+}
